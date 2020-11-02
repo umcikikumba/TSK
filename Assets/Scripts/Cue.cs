20e19271de716b8909ed78b6dc371f0b74513193
Bromonov <pawel.ryczkowski12@gmail.com>
2020-11-02 03:28:53 +0100

    new colliders, started working on gameplay

---
+++ Assets/Scripts/Cue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue : MonoBehaviour
+{
+    public float force = 10.0f;
+    public float speed = 1.0f;
+    public Transform whiteBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, whiteBall.position, step);
+        }
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if(collision.gameObject.layer == 8)         //"Balls" layer
+        {
+            collision.gameObject.SendMessage("ApplyForce", this);
+        }
+    }
+}
