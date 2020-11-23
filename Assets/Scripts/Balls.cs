0d450d361432f045aed032ddd7810abf2cd612b7
Bromonov <pawel.ryczkowski12@gmail.com>
2020-11-23 10:06:02 +0100

    fixed sliders for changing physic materials values

--- Assets/Scripts/Balls.cs
+++ Assets/Scripts/Balls.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Balls : MonoBehaviour
 {
+    public Slider bounce;
+    public Slider static_friction;
+    public Slider dynamic_friction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bounce.value = this.GetComponent<SphereCollider>().sharedMaterial.bounciness;
+        static_friction.value = this.GetComponent<SphereCollider>().sharedMaterial.staticFriction;
+        dynamic_friction.value = this.GetComponent<SphereCollider>().sharedMaterial.dynamicFriction;
     }
 
     // Update is called once per frame
@@ -18,21 +25,21 @@ public class Balls : MonoBehaviour
 
     public void AdjustBounciness(float newBounciness)
     {
-        this.GetComponent<Collider>().sharedMaterial.bounciness = newBounciness;
+        this.GetComponent<SphereCollider>().sharedMaterial.bounciness = newBounciness;
         gameObject.GetComponent<SphereCollider>().enabled = false;
         gameObject.GetComponent<SphereCollider>().enabled = true;
     }
 
     public void AdjustStaticFriction(float newStaticFriction)
     {
-        this.GetComponent<Collider>().sharedMaterial.staticFriction = newStaticFriction;
+        this.GetComponent<SphereCollider>().sharedMaterial.staticFriction = newStaticFriction;
         gameObject.GetComponent<SphereCollider>().enabled = false;
         gameObject.GetComponent<SphereCollider>().enabled = true;
     }
 
     public void AdjustDynamicFriction(float newDynamicFriction)
     {
-        this.GetComponent<Collider>().sharedMaterial.dynamicFriction = newDynamicFriction;
+        this.GetComponent<SphereCollider>().sharedMaterial.dynamicFriction = newDynamicFriction;
         gameObject.GetComponent<SphereCollider>().enabled = false;
         gameObject.GetComponent<SphereCollider>().enabled = true;
     }
--- Assets/Scripts/BilliardsTable.cs
+++ Assets/Scripts/BilliardsTable.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BilliardsTable : MonoBehaviour
 {
+    public Slider bounce;
+    public Slider static_friction;
+    public Slider dynamic_friction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bounce.value = this.GetComponent<Collider>().sharedMaterial.bounciness;
+        static_friction.value = this.GetComponent<Collider>().sharedMaterial.staticFriction;
+        dynamic_friction.value = this.GetComponent<Collider>().sharedMaterial.dynamicFriction;
     }
 
     // Update is called once per frame
@@ -18,21 +25,21 @@ public class BilliardsTable : MonoBehaviour
 
     public void AdjustBounciness(float newBounciness)
     {
-        this.GetComponent<Collider>().sharedMaterial.bounciness = newBounciness;
+        this.GetComponent<BoxCollider>().sharedMaterial.bounciness = newBounciness;
         gameObject.GetComponent<BoxCollider>().enabled = false;
         gameObject.GetComponent<BoxCollider>().enabled = true;
     }
 
     public void AdjustStaticFriction(float newStaticFriction)
     {
-        this.GetComponent<Collider>().sharedMaterial.staticFriction = newStaticFriction;
+        this.GetComponent<BoxCollider>().sharedMaterial.staticFriction = newStaticFriction;
         gameObject.GetComponent<BoxCollider>().enabled = false;
         gameObject.GetComponent<BoxCollider>().enabled = true;
     }
 
     public void AdjustDynamicFriction(float newDynamicFriction)
     {
-        this.GetComponent<Collider>().sharedMaterial.dynamicFriction = newDynamicFriction;
+        this.GetComponent<BoxCollider>().sharedMaterial.dynamicFriction = newDynamicFriction;
         gameObject.GetComponent<BoxCollider>().enabled = false;
         gameObject.GetComponent<BoxCollider>().enabled = true;
     }
