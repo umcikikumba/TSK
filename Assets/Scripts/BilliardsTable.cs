f1f3e4f5b2f06ba2eb7280d5d51abaf432c266e0
Bromonov <pawel.ryczkowski12@gmail.com>
2020-11-23 03:30:44 +0100

    added sliders for changing physic materials on balls and table, right now table bounciness and table static friction dont work, all other sliders work well

---
+++ Assets/Scripts/BilliardsTable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BilliardsTable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AdjustBounciness(float newBounciness)
+    {
+        this.GetComponent<Collider>().sharedMaterial.bounciness = newBounciness;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public void AdjustStaticFriction(float newStaticFriction)
+    {
+        this.GetComponent<Collider>().sharedMaterial.staticFriction = newStaticFriction;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public void AdjustDynamicFriction(float newDynamicFriction)
+    {
+        this.GetComponent<Collider>().sharedMaterial.dynamicFriction = newDynamicFriction;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/UIshowTextValue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIshowTextValue : MonoBehaviour
+{
+    Text text;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        showBouncinessValue();    
+    }
+
+    public void showBouncinessValue()
+    {
+        text.text = slider.value.ToString();
+    }
+}
